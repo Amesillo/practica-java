1083f101111a26aa5680dfa2dcb02aaf01a537d5
Amesillo <amepatitas18@gmail.com>
2026-03-20 00:18:14 -0600

    Update WebConfig.java

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,12 +1,23 @@
 package com.example.demo;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        // Fuerza a que la raíz cargue el index.html
         registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Asegura que Java entregue todos los archivos de Flutter (js, assets, etc)
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
